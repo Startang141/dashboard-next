91120cbfa142b5dcdef01f949f39dd7ea4afc308
Startang141 <bintangadiyatma141@gmail.com>
2025-10-21 14:24:23 +0700

    line charts

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { DollarSign, ShoppingBag, SquareActivity, Users } from "lucide-react";
 import StatCard from "@/components/StatCard";
+import SalesOverviewChart from "@/components/salesOverviewChart";
 
-const OverviewPage: React.FC = () => {
+const OverviewPage = () => {
   return (
     <div className="flex-1 overflow-auto relative z-10">
       <div className="max-w-screen mx-auto py-4 px-4 sm:px-6 lg:px-8 ">
@@ -12,6 +13,8 @@ const OverviewPage: React.FC = () => {
           <StatCard name="Total Product" icon={ShoppingBag} value="1324" />
           <StatCard name="Stock" icon={SquareActivity} value="98" />
         </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8"></div>
+        <SalesOverviewChart />
       </div>
     </div>
   );
